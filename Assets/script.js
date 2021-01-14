32a4c07f2e820861233fd2f125b710f1549a519a
Wanda Gleason <oglissane@gmail.com>
2021-01-14 14:36:38 -0500

    Working out some ideas still

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,100 @@
-## User Story
 
-```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
+    var quizContainer = document.getElementById("quiz");
+    var startButton = document.getElementById("startButton");
+    
 
-## Acceptance Criteria
+function showQuiz(){
+    // variable to store the HTML output
+    const output = [];
+    // for each question...
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
 
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
-```
+      // This ariable will store the list of possible answers for each question:
+      const answers = [];
 
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+}
+myQuestions.forEach( (currentQuestion, questionNumber) => {
+  // the code we want to run for each question goes here
+});
+function showResults(){}
+
+showQuiz();
+startButton.addEventListener('click', showResults);
+
+    const myQuestions = [
+      {
+        question: "Commonly used data types DO NOT include:",
+        answers: {
+          a: "strings",
+          b: "booleans",
+          c: "alerts",
+          d: "numbers",
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "The condition in an if / else statement is enclosed within ________.",
+        answers: {
+          a: "quotes",
+          b: "curly brackets",
+          c: "parentheses",
+          d: "square brackets"
+        }, 
+        correctAnswer: "c"
+      },
+      {
+        question: "Arrays in Javascript can be used to store ________.",
+        answers: {
+          a: "Numbers and strings",
+          b: "other arrays",
+          c: "booleans",
+          d: "All of the above"
+        },
+        correctAnswer: "d"
+      },
+      {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: {
+          a: "commas",
+          b: "curly brackets",
+          c: "quotes",
+          d: "parantheses",
+        },
+        correctAnswer: "c"
+      },
+      {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      answers: {
+        a: "JavaScript",
+        b: "terminal / bash",
+        c: "for loops",
+        d: "console.log",
+      },
+      correctAnswer: "c"
+    },
+    ];
+    quizContainer.innerHTML = output.join('');
