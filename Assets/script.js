cf00ae42ee08ef966f2386456a7dd412fb65081b
Wanda Gleason <oglissane@gmail.com>
2021-01-16 03:32:41 -0500

    Working basic timer and accessing the array of questions and answers, start button hides and launches game

--- Assets/script.js
+++ Assets/script.js
@@ -1,102 +1,125 @@
-$(function () {
+let codeQuizContainer = document.getElementById('codeQuizContainer');
+let resultsContainer = document.getElementById('userResults');
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
   console.log( "ready!" );
 });
-    var quizContainer = document.getElementById("quiz");
-    var startButton = document.getElementById("startButton");
-    
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-      // the code we want to run for each question goes here
-    });
-function showQuiz(){
-    // variable to store the HTML output
-    const output = [];
-    // for each question...
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-
-      // This ariable will store the list of possible answers for each question:
-      const answers = [];
-
-      // and for each available answer...
-      for(letter in currentQuestion.answers){
-
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
+document.getElementById('startButton').onclick = function startQuiz() 
+ {
+  // Rudimentary countdown timer
+var timeleft = 10; 
+var downloadTimer = setInterval(function(){ 
+  if(timeleft <= 0){ 
+    clearInterval(downloadTimer); 
+  } 
+  // adding progress bar so user can see time going by
+  document.getElementById("progressBar").value = 10 - timeleft; 
+  timeleft -= 1; }, 1000); 
+// Hiding the start button once it's clicked
+$('#startButton').hide();
+//hiding the current question
+$("#theCurrentQuestion").css('display', 'block');
+// adding variable to store output of the html
+let output = [];
+// so we will run through each question 
+questionsAndAnswers.forEach(
+  (currentQuestion, questionNumber) => {
+    // variable to store the list of possible answers to the question
+    let answers = [];
+    // this is for each possible answer
+    for(letter in currentQuestion.answers) {
+      // adding the radio buttons here vs on index.html
+      answers.push(
+        `<label>
+          <input type="radio" name="question${questionNumber}" value="${letter}"></input>
             ${letter} :
             ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
+        </label>`
       );
     }
-  );
-
+    output.push(
+      `<div class="question"> ${currentQuestion.question}</div>
+      <div class ="answers"> ${answers.join('')} </div>`
+    );
+  }
+);
   // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
+  codeQuizContainer.innerHTML = output.join('');
 }
-
-function showResults(){}
-
-showQuiz();
-startButton.addEventListener('click', showResults);
-
-    const myQuestions = [
-      {
-        question: "Commonly used data types DO NOT include:",
-        answers: {
-          a: "strings",
-          b: "booleans",
-          c: "alerts",
-          d: "numbers",
-        },
-        correctAnswer: "c"
-      },
-      {
-        question: "The condition in an if / else statement is enclosed within ________.",
-        answers: {
-          a: "quotes",
-          b: "curly brackets",
-          c: "parentheses",
-          d: "square brackets"
-        }, 
-        correctAnswer: "c"
-      },
-      {
-        question: "Arrays in Javascript can be used to store ________.",
-        answers: {
-          a: "Numbers and strings",
-          b: "other arrays",
-          c: "booleans",
-          d: "All of the above"
-        },
-        correctAnswer: "d"
-      },
-      {
-        question: "String values must be enclosed within ________ when being assigned to variables.",
-        answers: {
-          a: "commas",
-          b: "curly brackets",
-          c: "quotes",
-          d: "parantheses",
-        },
-        correctAnswer: "c"
-      },
-      {
-      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-      answers: {
-        a: "JavaScript",
-        b: "terminal / bash",
-        c: "for loops",
-        d: "console.log",
-      },
-      correctAnswer: "c"
+var questionsAndAnswers = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: {
+      A: "strings",
+      B: "booleans",
+      C: "alerts",
+      D: "numbers",
     },
-    ];
-    quizContainer.innerHTML = output.join('');
+    correctAnswer: "C"
+  },
+  {
+    question: "The condition in an if / else statement is enclosed within ________.",
+    answers: {
+      A: "quotes",
+      B: "curly brackets",
+      C: "parentheses",
+      D: "square brackets"
+    }, 
+    correctAnswer: "C"
+  },
+  {
+    question: "Arrays in Javascript can be used to store ________.",
+    answers: {
+      A: "Numbers and strings",
+      B: "other arrays",
+      C: "booleans",
+      D: "All of the above"
+    },
+    correctAnswer: "D"
+  },
+  {
+    question: "String values must be enclosed within ________ when being assigned to variables.",
+    answers: {
+      A: "commas",
+      B: "curly brackets",
+      C: "quotes",
+      D: "parantheses",
+    },
+    correctAnswer: "C"
+  },
+  {
+  question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  answers: {
+    A: "JavaScript",
+    B: "terminal / bash",
+    C: "for loops",
+    D: "console.log",
+  },
+  correctAnswer: "C"
+},
+];
+function showResults(){
+
+  // make a variable to "hold" the answers from our quiz
+  let answerContainers = codeQuizContainer.querySelectorAll('.answers');
+  // keep track of user's answers - user of course starts off with 0 correct
+  let numCorrect = 0;
+
+  // for each question...
+  myQuestions.forEach( (currentQuestion, questionNumber) => {
+    // find the selected answer
+    let answerContainer = answerContainers[questionNumber];
+    let selector = `input[name=question${questionNumber}]:checked`;
+    let userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    // if the user's answer is correct
+    if(userAnswer === currentQuestion.correctAnswer)
+    {
+      // add 1 to the number of correct answers
+      numCorrect++;
+    }
+  });
+  // show number of correct answers out of total number possible
+  resultsContainer.innerHTML = `${numCorrect} out of ${questionsAndAnswers.length}`;
+};
+    
